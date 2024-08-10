6db4a85999ffd981de3a2c97da1afe726c319c17
Antoine Massias <massias.antoine.pro@gmail.com>
2024-08-10 18:10:58 +0200

    Files are now sorted.
    Added:
    	- The files in the list are now sorted in
    		alphabetical order.

--- Files.mk
+++ Files.mk
@@ -1,4 +1,4 @@
 FILES	=																	\
-	very_long_name1 very_long_name7 very_long_name3 very_long_name4			\
-	very_long_name9 very_long_name5 very_long_name10 very_long_name8		\
-	very_long_name6 very_long_name0 very_long_name2							\
+	very_long_name0 very_long_name1 very_long_name10 very_long_name2		\
+	very_long_name3 very_long_name4 very_long_name5 very_long_name6			\
+	very_long_name7 very_long_name8 very_long_name9							\
